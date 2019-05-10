9ec5d23d6a142a01a45b082a2dd687c3c45e6edc
tyj <atangyj@gmail.com>
2019-05-10 09:38:44 +0100

    Updates

---
+++ precache-manifest.3c28daa6c92641565e1f20f7596972ed.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6956448e953b4f7f8599f04cb3f6576",
+    "url": "/doopollheroes/index.html"
+  },
+  {
+    "revision": "36ae175e8f744e3a2ff4",
+    "url": "/doopollheroes/static/css/main.6f82d709.chunk.css"
+  },
+  {
+    "revision": "e9f51867d711cdfcf3f6",
+    "url": "/doopollheroes/static/js/2.5ceca9b3.chunk.js"
+  },
+  {
+    "revision": "36ae175e8f744e3a2ff4",
+    "url": "/doopollheroes/static/js/main.6b041d84.chunk.js"
+  },
+  {
+    "revision": "50810fdb3c0dbc9f68e9",
+    "url": "/doopollheroes/static/js/runtime~main.84d1ff2c.js"
+  },
+  {
+    "revision": "4743b6c8ad57033e21c5b93374423215",
+    "url": "/doopollheroes/static/media/batman.4743b6c8.png"
+  },
+  {
+    "revision": "5209cbfb0f50266fb55ab513cd73b782",
+    "url": "/doopollheroes/static/media/batman.5209cbfb.png"
+  },
+  {
+    "revision": "c9c735808d38a5a8b1b99963ae87d6d0",
+    "url": "/doopollheroes/static/media/birdy.c9c73580.png"
+  },
+  {
+    "revision": "20f48f090107b22fbd07a0294aa28268",
+    "url": "/doopollheroes/static/media/catwoman.20f48f09.png"
+  },
+  {
+    "revision": "e09379cecccbd4a736d40f2f0a3f6e5e",
+    "url": "/doopollheroes/static/media/catwoman.e09379ce.png"
+  },
+  {
+    "revision": "c118ceb879fc0e85db03492d48201807",
+    "url": "/doopollheroes/static/media/doge.c118ceb8.png"
+  },
+  {
+    "revision": "96bb46ec10285782c02f199511ab8112",
+    "url": "/doopollheroes/static/media/raccoon.96bb46ec.png"
+  },
+  {
+    "revision": "70a77782c05e9016794c775edf07a2b0",
+    "url": "/doopollheroes/static/media/thor.70a77782.png"
+  },
+  {
+    "revision": "e2d4e95a53da5ee1f38331c007e1f8ef",
+    "url": "/doopollheroes/static/media/thor.e2d4e95a.png"
+  }
+]);
